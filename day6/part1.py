8fc44ce876291f7f30592254c6a98ab606e0b031
Aayush Borkar <aayushb14@gmail.com>
2023-12-07 11:06:01 +0530

    Derp

--- day6/part1.py
+++ day6/part1.py
@@ -1,11 +1,16 @@
 from math import floor, ceil, pow, sqrt
 
 f = open("input", "r")
-t = int("".join([char for char in f.readline().rstrip() if char.isdigit()]))
-d = int("".join([char for char in f.readline().rstrip() if char.isdigit()]))
+times = [int(num) for num in f.readline().rstrip().split() if num.isdigit()]
+distances = [int(num) for num in f.readline().rstrip().split() if num.isdigit()]
 f.close()
-a = ceil((t - sqrt(pow(t, 2) - 4 * d)) / 2)
-b = floor((t + sqrt(pow(t, 2) - 4 * d)) / 2)
-ans = b - a + 1
+ans = 1
+for idx, (t, d) in enumerate(zip(times, distances)):
+    if pow(t, 2) < 4 * d:
+        ans *= 0
+        break
+    a = ceil((t - sqrt(pow(t, 2) - 4 * d)) / 2)
+    b = floor((t + sqrt(pow(t, 2) - 4 * d)) / 2)
+    ans *= b - a + 1
 print(ans)
 
